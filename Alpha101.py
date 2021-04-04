dfe5349ff5f05c486db8857615991ade3794bca3
Emilio Basualdo <Pilobas97@gmail.com>
2021-04-04 17:37:32 -0300

    csv saving

--- Alpha101.py
+++ Alpha101.py
@@ -1,20 +1,36 @@
+import logging
+import sys
+from datetime import datetime
 from math import floor
-
 import numpy as np
+import os
 import pandas as pd
 import vectorbt as vbt
-from vectorbt.base.reshape_fns import to_2d, to_1d
-from vectorbt.utils.config import merge_dicts
 from numba import njit
-from vectorbt.utils.decorators import cached_property, custom_method
+from vectorbt.utils.decorators import custom_method
+
 from utils import file_to_data_frame
 
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+
 if __name__ == '__main__':
+    start_time = datetime.now()
+    RESULTS_FILE_PREFIX = f"./results/{start_time.strftime('%Y-%m-%d_%H:%M:%S')}"
+    if not os.path.exists(RESULTS_FILE_PREFIX):
+        os.makedirs(RESULTS_FILE_PREFIX)
 
-    (s_name, data) = file_to_data_frame(
-        "/Users/pilo/development/itba/pf/Binance_Minute_OHLC_CSVs/Binance_BNBUSDT_minute.csv")
+    filename = "/Users/pilo/development/itba/pf/Binance_Minute_OHLC_CSVs/shorts/Binance_ADAUSDT_minute_3000.csv"
+    logging.info(f"Reading file {filename}")
+    (s_name, data) = file_to_data_frame(filename)
     price = data["Close"]
-
 
     split_kwargs = dict(
         n=5,
@@ -23,10 +39,12 @@ if __name__ == '__main__':
         left_to_right=False
     )  # 30 windows, each 2 years long, reserve 180 days for test
     portfolio_kwargs = dict(
-        direction='all',  # long and short
-        freq='d'
+        direction='all',  # long and short todo falta ver esto
+        freq='d' # todo falta ver esto
     )
     windows = np.arange(10, 50)
+
+    logging.info(f"Rolling window with params:\nsplit_kwargs={split_kwargs}\nportfolio_kwargs={portfolio_kwargs}")
 
     def roll_in_and_out_samples(price, **kwargs):
         return price.vbt.rolling_split(**kwargs)
@@ -49,7 +67,6 @@ if __name__ == '__main__':
 
     in_hold_elr = simulate_holding(in_price, **portfolio_kwargs)
 
-
     def simulate_all_params(price, windows, **kwargs):
         fast_ma, slow_ma = vbt.MA.run_combs(price, windows, r=2, short_names=['fast', 'slow'])
         entries = fast_ma.ma_above(slow_ma, crossover=True)
@@ -58,18 +75,14 @@ if __name__ == '__main__':
         return portfolio.expected_log_returns()
 
     # Simulate all params for in-sample ranges
-    in_elr = simulate_all_params(in_price, windows, **portfolio_kwargs)
-
-    print(in_elr)
+    in_elr_hold = simulate_all_params(in_price, windows, **portfolio_kwargs)
 
     def get_best_index(performance, higher_better=True):
         if higher_better:
             return performance[performance.groupby('split_idx').idxmax()].index
         return performance[performance.groupby('split_idx').idxmin()].index
 
-    in_best_index = get_best_index(in_elr)
-
-    print(in_best_index)
+    in_best_index = get_best_index(in_elr_hold)
 
     def get_best_params(best_index, level_name):
         return best_index.get_level_values(level_name).to_numpy()
@@ -78,18 +91,10 @@ if __name__ == '__main__':
     in_best_slow_windows = get_best_params(in_best_index, 'slow_window')
     in_best_window_pairs = np.array(list(zip(in_best_fast_windows, in_best_slow_windows)))
 
-    print(in_best_window_pairs)
-
-    pd.DataFrame(in_best_window_pairs, columns=['fast_window', 'slow_window']).vbt.plot().show()
-
     out_hold_elr = simulate_holding(out_price, **portfolio_kwargs)
-
-    print(out_hold_elr)
 
     # Simulate all params for out-sample ranges
     out_elr = simulate_all_params(out_price, windows, **portfolio_kwargs)
-
-    print(out_elr)
 
     def simulate_best_params(price, best_fast_windows, best_slow_windows, **kwargs):
         fast_ma = vbt.MA.run(out_price, window=best_fast_windows, per_column=True)
@@ -102,24 +107,27 @@ if __name__ == '__main__':
     # Use best params from in-sample ranges and simulate them for out-sample ranges
     out_test_elr = simulate_best_params(out_price, in_best_fast_windows, in_best_slow_windows, **portfolio_kwargs)
 
-    print(out_test_elr)
+    pd.DataFrame(in_best_window_pairs, columns=['fast_window', 'slow_window']).to_csv(
+        f"{RESULTS_FILE_PREFIX}/best_params_combination.csv")
 
-    cv_results_df = pd.DataFrame({
+    pd.DataFrame({
         'in_sample_hold': in_hold_elr.values,
-        'in_sample_median': in_elr.groupby('split_idx').median().values,
-        'in_sample_best': in_elr[in_best_index].values,
+        #'in_sample_median': in_elr_hold.groupby('split_idx').median().values,
+        'in_sample_best': in_elr_hold[in_best_index].values,
         'out_sample_hold': out_hold_elr.values,
-        'out_sample_median': out_elr.groupby('split_idx').median().values,
+       # 'out_sample_median': out_elr.groupby('split_idx').median().values,
         'out_sample_test': out_test_elr.values
-    })
+    }).to_csv(f"{RESULTS_FILE_PREFIX}/final_strat-vs-hold.csv")
 
-    cv_results_df.vbt.plot(
-        trace_kwargs=[
-            dict(line_color=vbt.settings.color_schema['blue']),
-            dict(line_color=vbt.settings.color_schema['blue'], line_dash='dash'),
-            dict(line_color=vbt.settings.color_schema['blue'], line_dash='dot'),
-            dict(line_color=vbt.settings.color_schema['orange']),
-            dict(line_color=vbt.settings.color_schema['orange'], line_dash='dash'),
-            dict(line_color=vbt.settings.color_schema['orange'], line_dash='dot')
-        ]
-    ).show()
+    logging.info(f"Time taken {datetime.now() - start_time}")
+
+    # cv_results_df.vbt.plot(
+    #     trace_kwargs=[
+    #         dict(line_color=vbt.settings.color_schema['blue']),
+    #         dict(line_color=vbt.settings.color_schema['blue'], line_dash='dash'),
+    #         dict(line_color=vbt.settings.color_schema['blue'], line_dash='dot'),
+    #         dict(line_color=vbt.settings.color_schema['orange']),
+    #         dict(line_color=vbt.settings.color_schema['orange'], line_dash='dash'),
+    #         dict(line_color=vbt.settings.color_schema['orange'], line_dash='dot')
+    #     ]
+    # ).show()
