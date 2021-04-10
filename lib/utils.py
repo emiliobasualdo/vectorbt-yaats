77831d5a26f2aad4cd9349ba1f6d19a4305b2e5d
Emilio Basualdo <Pilobas97@gmail.com>
2021-04-10 13:29:07 -0300

    bug

--- lib/utils.py
+++ lib/utils.py
@@ -7,6 +7,7 @@ from math import floor
 from pathlib import Path
 from typing import List, Tuple
 
+import numpy as np
 import pandas as pd
 import vectorbt as vbt
 from numba import njit
@@ -19,7 +20,11 @@ class ExtendedPortfolio(Portfolio):
     @custom_method
     def expected_log_returns(self):
         """Get log return mean series per column/group based on portfolio value."""
-        log_nb = njit(lambda col, pnl: log(pnl/100 + 1))
+        @njit
+        def log_nb(col, pnl):
+            pnl = pnl[~np.isnan(pnl)]
+            return log(pnl/100 + 1)
+        #log_nb = njit(lambda col, pnl: log(pnl/100 + 1))
         mean_nb = njit(lambda col, l_rets: nanmean(l_rets))
         return self.trades.pnl.to_matrix().vbt.apply_and_reduce(log_nb, mean_nb, wrap_kwargs=dict(name_or_index="expected_log_returns"))
 
@@ -65,11 +70,11 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-def create_windows(ohlc: pd.Series, n=5, window_len=0.6, training_set_len=0.4) -> ((), ()):
+def create_windows(ohlc: pd.Series, n=5, window_len=0.6, right_set_len=0.4) -> ((), ()):
     split_kwargs = dict(
         n=n,
         window_len=floor(len(ohlc) * window_len),
-        set_lens=(training_set_len,),
+        set_lens=(window_len * right_set_len,),
         left_to_right=False
     )  # n windows, each window_len long, reserve training_set_len days for test/training(nomeacuerdo) #todo revisar
 
@@ -80,105 +85,3 @@ def create_windows(ohlc: pd.Series, n=5, window_len=0.6, training_set_len=0.4) -
     fig = ohlc.vbt.rolling_split(**split_kwargs)
     return fig, windows
 
-
-def window_cross_validation(dirname, ):
-    # Parseamos los argumentos de línea de comando
-    parser = argparse.ArgumentParser(description='Simulate your stuff')
-    parser.add_argument('-f', '--ohlc_file_path', type=str)
-    parser.add_argument('-l', '--log_to_file', action="store_true")
-    parser.add_argument('-p', '--plot', action="store_true")
-    parser.add_argument('-c', '--save_csv', action="store_true")
-    args = parser.parse_args()
-    ohlc_file_path = args.ohlc_file_path
-    log_to_file = args.ohlc_file_path
-    plot = args.ohlc_file_path
-    save_to_csv = args.save_csv
-
-    start_time = datetime.now()
-
-    if log_to_file or plot or save_to_csv:
-        RESULTS_FILE_PREFIX = f"./results/{dirname}/{start_time.strftime('%Y-%m-%d_%H:%M:%S')}"
-        # Creamos la carpeta donde se van a guardar los resultados/logs
-        # todo guardar en S3 para poder apagar la máquina
-        if not os.path.exists(RESULTS_FILE_PREFIX):
-            os.makedirs(RESULTS_FILE_PREFIX)
-        if log_to_file:
-            # Podemos guardar los logs en un archivo
-            fh = logging.FileHandler(f"{RESULTS_FILE_PREFIX}/logs.log")
-            formatter = logging.Formatter('%(asctime)s - %(message)s')
-            fh.setFormatter(formatter)
-            logger.addHandler(fh)
-
-    # leemos el csv
-    logging.info(f"Reading file {ohlc_file_path}")
-    (s_name, data) = file_to_data_frame(ohlc_file_path)
-    price = data["Close"]
-
-    # creamos las ventanas
-    (training_price, train_indexes), (test_price, test_indexes) = create_windows(ohlc=price, plot=True)
-
-    # simulamos Buy&Hold de cada training y test window y tomamo el expected log returns (elr)
-    portfolio_kwargs = dict(
-        direction='all',  # long and short todo falta ver esto
-        freq='d'  # todo falta ver esto
-    )
-    in_hold_elr = ExtendedPortfolio.from_holding(training_price, **portfolio_kwargs).expected_log_returns()
-    out_hold_elr = ExtendedPortfolio.from_holding(test_price, **portfolio_kwargs).expected_log_returns()
-
-    # Simulamos con el training set y el rango de parámetros
-    params_range = arange(10, 50)
-    training_elr = optimize(training_price, params_range, **portfolio_kwargs).expected_log_returns()
-
-    # Podemos simular el test set para buscar el cúal serían las mejores configuraciónes para cada ventana
-    optimize_test_set = False
-    if optimize_test_set:
-        out_elr = optimize(test_price, params_range, **portfolio_kwargs).expected_log_returns()
-
-    def get_best_index(performance, higher_better=True):
-        if higher_better:
-            return performance[performance.groupby('split_idx').idxmax()].index
-        return performance[performance.groupby('split_idx').idxmin()].index
-
-    def get_best_params(best_index, level_name):
-        return best_index.get_level_values(level_name).to_numpy()
-
-    # Buscamos los mejores índices resultado de cada training window
-    training_best_index = get_best_index(training_elr)
-    # Buscamos los mejores parámetros por ventana
-    training_best_fast_windows = get_best_params(training_best_index, 'fast_window')
-    training_best_slow_windows = get_best_params(training_best_index, 'slow_window')
-    training_best_window_pairs = np.array(list(zip(training_best_fast_windows, training_best_slow_windows)))
-
-    # Simulamos con el test set y los mejores parámetros de cada ventana de training set
-    test_elr = simulate(test_price, training_best_fast_windows, training_best_slow_windows,
-                        **portfolio_kwargs).expected_log_returns()
-
-    final_result = pd.DataFrame({
-        'in_sample_hold': in_hold_elr.values,
-        # 'in_sample_median': training_elr.groupby('split_idx').median().values,
-        'in_sample_best': training_elr[training_best_index].values,
-        'out_sample_hold': out_hold_elr.values,
-        # 'out_sample_median': out_elr.groupby('split_idx').median().values,
-        'out_sample_test': test_elr.values
-    })
-
-    if save_to_csv:
-        # guardamos los mejores parámetros por ventana
-        pd.DataFrame(training_best_window_pairs, columns=['fast_window', 'slow_window']).to_csv(
-            f"{RESULTS_FILE_PREFIX}/best_params_combination.csv")
-        # guardamos los resultados finales
-        final_result.to_csv(f"{RESULTS_FILE_PREFIX}/final_strat-vs-hold.csv")
-
-    logging.info(f"Time taken {datetime.now() - start_time}")
-
-    if plot:
-        final_result.vbt.plot(
-            trace_kwargs=[
-                dict(line_color=vbt.settings.color_schema['blue']),
-                dict(line_color=vbt.settings.color_schema['blue'], line_dash='dash'),
-                dict(line_color=vbt.settings.color_schema['blue'], line_dash='dot'),
-                dict(line_color=vbt.settings.color_schema['orange']),
-                dict(line_color=vbt.settings.color_schema['orange'], line_dash='dash'),
-                dict(line_color=vbt.settings.color_schema['orange'], line_dash='dot')
-            ]
-        )
--- strategies/Alpha101.py
+++ strategies/Alpha101.py
@@ -1,6 +1,10 @@
 #%%
-from datetime import datetime
 
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
 from numba import njit
 import numpy as np
 from lib.utils import create_windows, file_to_data_frame, ExtendedPortfolio
@@ -15,16 +19,17 @@ import vectorbt as vbt
 # agarramos solo las columnas que necesitamos
 cols = ohlcv.columns
 print(cols)
-#ohlcv.get(["Open", "High", "Low", "Close", "Volume"]).vbt.ohlcv.plot().show_png()
 ohlc = ohlcv.get(["Open", "High", "Low", "Close"])
 print(ohlc.head())
 
 #%%
 
 # creamos las ventanas
-figure, windows = create_windows(ohlc=ohlc, n=8, window_len=0.6, training_set_len=0.4)
+figure, windows = create_windows(ohlc=ohlc, n=2, window_len=0.5, right_set_len=0.5)
 (in_df, in_df_index), (out_df, _) = windows
-#figure.show()
+
+#%%
+
 
 #%%
 
@@ -36,7 +41,6 @@ portfolio_kwargs = dict(
     direction='longonly',
     freq='m'
 )
-
 
 #%%
 
@@ -58,27 +62,30 @@ AlphaInd = vbt.IndicatorFactory(
 
 #%%
 
-# Corremos con todas las combinaciones
-def simulate_all_params(ohlc_windows, params_range):
+def simulate(ohlc_windows, buy_threshold, sell_threshold, param_product):
     # creamos las señales
     open = ohlc_windows.xs("Open", level=1, axis=1)
     high = ohlc_windows.xs("High", level=1, axis=1)
     low = ohlc_windows.xs("Low", level=1, axis=1)
     close = ohlc_windows.xs("Close", level=1, axis=1)
     momentum = AlphaInd.run(open=open, high=high, low=low, close=close,
-                            buy_threshold=params_range, sell_threshold=params_range,
-                            param_product=True,
+                            buy_threshold=buy_threshold, sell_threshold=sell_threshold,
+                            param_product=param_product,
+                            per_column=not param_product,
                             short_name="alpha")
-    ones = np.full(momentum.signal.shape, 1)
-    entry_signal = momentum.signal_equal(ones)
-    exit_signal = momentum.signal_equal(-ones)
+    ones = np.full(momentum.signal.shape[-1], 1)
+    entry_signal = momentum.signal_equal(ones, crossover=True)
+    exit_signal = momentum.signal_equal(-ones, crossover=True)
     return ExtendedPortfolio.from_signals(close, entry_signal, exit_signal, **portfolio_kwargs)
 
 #%%
 
-# Optimizamos para el in y el out
-params_range = np.linspace(0.1, 1, 90, endpoint=False)
-in_elr = simulate_all_params(in_df, params_range).expected_log_returns()
+# Optimizamos para el in
+buy_range = np.linspace(0.1, 0.9, 10, endpoint=False)
+sell_range = np.linspace(0.7, 1, 10, endpoint=False)
+in_port = simulate(in_df, buy_range, sell_range, param_product=True)
+in_sharpe = in_port.sharpe_ratio()
+in_elr = in_port.expected_log_returns()
 #out_elr = simulate_all_params(out_df, params_range).expected_log_returns()
 
 #%%
@@ -95,43 +102,26 @@ def get_best_params(best_index, level_name):
 
 #Buscamos el índice de los mejores resultados del in
 in_best_index = get_best_index(in_elr)
-print(in_best_index)
-
-#%%
-
 in_best_buy_thresholds = get_best_params(in_best_index, 'alpha_buy_threshold')
 in_best_sell_thresholds = get_best_params(in_best_index, 'alpha_sell_threshold')
 in_best_threshold_pairs = np.array(list(zip(in_best_buy_thresholds, -in_best_sell_thresholds)))
 
-print(in_best_threshold_pairs)
+#%%
+
 
 #%%
 
-#pd.DataFrame(in_best_threshold_pairs, columns=['buy_threshold', 'sell_threshold']).vbt.plot().show()
-
-#%%
-
-# Corremos el out con los mejores parámetros de in
-#close, high, low, open = list(map(lambda tu: tu[1], in_df.groupby(level=1, axis=1)))
-open = in_df.xs("Open", level=1, axis=1)
-high = in_df.xs("High", level=1, axis=1)
-low = in_df.xs("Low", level=1, axis=1)
-close = in_df.xs("Close", level=1, axis=1)
-momentum = AlphaInd.run(open=open, high=high, low=low, close=close,
-                        buy_threshold=in_best_buy_thresholds, sell_threshold=in_best_sell_thresholds,
-                        short_name="alpha", per_column=True)
-ones = np.full(momentum.signal.shape[-1], 1)
-entry_signal = momentum.signal_equal(ones, crossover=True)
-exit_signal = momentum.signal_equal(-ones, crossover=True)
-# imprimo para confirmar que haya algún true
-entry_signal.loc[entry_signal[in_best_buy_thresholds[0], in_best_sell_thresholds[0], 0]==True].head()
-
-#%%
-
-trade_price = close
-out_test_port = ExtendedPortfolio.from_signals(trade_price, entry_signal, exit_signal, **portfolio_kwargs)
+out_test_port = simulate(out_df, in_best_buy_thresholds, in_best_sell_thresholds, param_product=False)
 out_test_elr = out_test_port.expected_log_returns()
-print(out_test_elr)
+out_test_sharpe = out_test_port.sharpe_ratio()
+
+#%%
+
+pnl1 = in_port.trades.pnl.to_matrix()[in_best_index]
+pnl1 = pnl1[np.isfinite(pnl1).all(axis=1)]
+pnl2 = out_test_port.trades.pnl.to_matrix()
+pnl2 = pnl2[np.isfinite(pnl2).all(axis=1)]
+assert(np.array_equal(pnl1.columns,pnl2.columns))
 
 #%%
 
@@ -139,37 +129,33 @@ print(out_test_elr)
 close_columns = list(filter(lambda col: "Close" in col[1], in_df.columns))
 in_hold_port = ExtendedPortfolio.from_holding(in_df[close_columns], **portfolio_kwargs)
 out_hold_port = ExtendedPortfolio.from_holding(out_df[close_columns], **portfolio_kwargs)
-print(in_hold_port.trades.values[:4])
-print(out_hold_port.trades.values[:4])
+assert (in_hold_port.trades.values[0]) # por lo menos 1 trade
+assert (out_hold_port.trades.values[0])
+
 in_hold_elr = in_hold_port.expected_log_returns()
+in_hold_sharpe = in_hold_port.sharpe_ratio()
 out_hold_elr = out_hold_port.expected_log_returns()
-print(in_hold_elr, out_hold_elr)
+out_hold_sharpe = out_hold_port.sharpe_ratio()
 
 #%%
 
+print(f"¿Todos los elrs son iguales?:{in_elr[in_best_index].values == out_test_elr.values}")
+#%%
+# ploteamos los elrs
 cv_results_df = pd.DataFrame({
     'in_sample_hold': in_hold_elr.values,
-    #'in_sample_median': in_elr.groupby('split_idx').median().values,
     'in_sample_best': in_elr[in_best_index].values,
     'out_sample_hold': out_hold_elr.values,
-    #'out_sample_median': out_elr.groupby('split_idx').median().values,
     'out_sample_test': out_test_elr.values
-}).to_csv(f"./result-{datetime.now()}.csv")
-#
-# cv_results_df.vbt.plot(
-#     trace_kwargs=[
-#         dict(line_color=vbt.settings.color_schema['blue']),
-#         dict(line_color=vbt.settings.color_schema['blue'], line_dash='dash'),
-#         dict(line_color=vbt.settings.color_schema['blue'], line_dash='dot'),
-#         dict(line_color=vbt.settings.color_schema['orange']),
-#         dict(line_color=vbt.settings.color_schema['orange'], line_dash='dash'),
-#         dict(line_color=vbt.settings.color_schema['orange'], line_dash='dot')
-#     ]
-# ).show()
+})
+
 
 #%%
 
-# for col in in_best_index:
-#     out_test_port.trades.plot(column=col).show()
-# out_test_port.trades.plot_pnl(column=col).show()
-
+#ploteamos los sharps
+cv_results_df = pd.DataFrame({
+    'in_sample_hold': in_hold_sharpe.values,
+    'in_sample_best': in_sharpe[in_best_index].values,
+    'out_sample_hold': out_hold_sharpe.values,
+    'out_sample_test': out_test_sharpe.values
+})
